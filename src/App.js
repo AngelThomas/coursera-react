2d311411da393221ad662d965ccbaa997f7d4d76
Thomas Angel <angel.thomas@gmx.net>
2020-03-26 10:24:44 +0100

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 
 /* function App() {
@@ -28,6 +29,14 @@ import Menu from './components/MenuComponent';
 } */
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -36,7 +45,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
