303ff9d4df4951382a449cccb30565c563c6405f
Thomas Angel <angel.thomas@gmx.net>
2020-04-14 10:29:26 +0200

    React Router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Main from './components/MainComponent';
-
+import { BrowserRouter } from 'react-router-dom';
 
 
 class App extends Component {
@@ -10,9 +10,11 @@ class App extends Component {
 
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
         <Main />
       </div>
+      </BrowserRouter>
     );
   }
 }
